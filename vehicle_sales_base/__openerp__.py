2b375ecc0e8cf130836d339b2290b2613686156d
Jeffery Chen Fan <jeffery9@gmail.com>
2015-07-04 11:44:19 +0800

    自定义角色

---
+++ vehicle_sales_base/__openerp__.py
@@ -0,0 +1,42 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "汽车销售基础",
+
+    'summary': """
+        本模块定义了汽车销售解决方案的基础数据
+
+        """,
+
+    'description': """
+        本模块定义了汽车销售解决方案的基础数据
+        1，角色
+        2，权限
+        3，菜单系统
+
+
+
+    """,
+
+    'author': "Your Company",
+    'website': "http://www.yourcompany.com",
+
+    # Categories can be used to filter modules in modules listing
+    # Check https://github.com/odoo/odoo/blob/master/openerp/addons/base/module/module_data.xml
+    # for the full list
+    'category': 'Uncategorized',
+    'version': '0.1',
+
+    # any module necessary for this one to work correctly
+    'depends': ['crm', 'sale'],
+
+    # always loaded
+    'data': [
+        # 'security/ir.model.access.csv',
+        'templates.xml',
+        'security/roles.xml',
+    ],
+    # only loaded in demonstration mode
+    'demo': [
+        'demo.xml',
+    ],
+}
