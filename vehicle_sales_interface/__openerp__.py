7a6eaee6b19f4e46f654d270d38e59ac239db674
Jeffery Chen Fan <jeffery9@gmail.com>
2015-07-07 13:04:52 +0800

    修改销售菜单

---
+++ vehicle_sales_interface/__openerp__.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "汽车销售界面",
+
+    'summary': """
+        本模块定义了汽车销售解决方案的界面
+
+        """,
+
+    'description': """
+        本模块定义了汽车销售解决方案的界面
+        1，菜单
+        2，
+
+
+
+    """,
+
+    'author': "Your Company",
+    'website': "http://www.yourcompany.com",
+
+    'category': 'Uncategorized',
+    'version': '0.1',
+
+    'depends': ['crm', 'sale', 'purchase', 'account_accountant', 'stock'],
+
+    'data': [
+        'templates.xml',
+    ],
+}
---
+++ vehicle_sales_interface/models.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+
+from openerp.osv import osv
+from openerp import SUPERUSER_ID
+from lxml import etree
+from lxml.builder import E
+from openerp.tools.translate import _
+from openerp.addons.base.res.res_users import name_boolean_group, name_selection_groups
+
+
