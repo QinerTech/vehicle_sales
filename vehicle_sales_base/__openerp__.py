32ce1f4519f994b5cfe17cd12c7b7fae830c3dc0
Jeffery Chen Fan <jeffery9@gmail.com>
2015-07-16 19:38:34 +0800

    调整 基础数据和设置

--- vehicle_sales_base/__openerp__.py
+++ vehicle_sales_base/__openerp__.py
@@ -27,11 +27,11 @@
     'version': '0.1',
 
     # any module necessary for this one to work correctly
-    'depends': ['crm', 'sale'],
+    'depends': ['crm', 'sale', 'purchase', 'account_accountant', 'stock'],
 
     # always loaded
     'data': [
-        # 'security/ir.model.access.csv',
+        'data.xml',
         'templates.xml',
         'security/roles.xml',
     ],
